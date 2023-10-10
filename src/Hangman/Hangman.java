1a8443f24230e7b5594995cb72bf6592f6f4e4c7
mykytas <m.o.strelnyk@student.khai.edu>
2023-10-10 10:45:21 +0300

    Hangman stage 2

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,8 +1,23 @@
 package Hangman;
+import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon");
+        String correctAnswer = "java";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the word: ");
+        String userAnswer = scanner.nextLine();
+        if (userAnswer.equals(correctAnswer)) {
+            System.out.println("Congratulations!");
+        } else {
+            while (!userAnswer.equals(correctAnswer)) {
+                System.out.println("Wrong! Try again ");
+                System.out.print("Enter the word: ");
+                userAnswer = scanner.nextLine();
+            }
+            System.out.println("Congratulations!");
+        }
     }
 }
