73f0b6bc551c15f0b78e588d19b4c3f68e27514d
mykytas <m.o.strelnyk@student.khai.edu>
2023-10-10 10:55:57 +0300

    Hangman stage 3

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,11 +1,15 @@
 package Hangman;
 import java.util.Scanner;
+import java.util.Random;
 
 public class Hangman {
     public static void main(String[] args) {
         System.out.println("HANGMAN");
         System.out.println("The game will be available soon");
-        String correctAnswer = "java";
+        String [] answers = {"python", "java", " javascript", "kotlin"};
+        Random random = new Random();
+        int select = random.nextInt(answers.length);
+        String correctAnswer = answers[select];
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the word: ");
         String userAnswer = scanner.nextLine();
