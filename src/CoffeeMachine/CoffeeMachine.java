63acfdf85a7ede32666297013fee56edb04d5312
mykytas <m.o.strelnyk@student.khai.edu>
2023-10-16 10:02:03 +0300

    stage 1-2 done

---
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -0,0 +1,18 @@
+package CoffeeMachine;
+import java.util.Scanner;
+public class CoffeeMachine {
+    public static void main (String args[]){
+        basicCupCalc();
+    }
+
+    static void basicCupCalc() {
+        System.out.print("Write how many cups you want: ");
+        Scanner scanner = new Scanner(System.in);
+        String nCups = scanner.nextLine();
+        int answer = Integer.parseInt(nCups);
+        System.out.println(String.format("For %d cups of coffee you will need ", answer));
+        System.out.println(String.format("%d ml of water", answer * 200));
+        System.out.println(String.format("%d ml of milk", answer * 50));
+        System.out.println(String.format("%d g of beans", answer * 15));
+    }
+}
