f020d90c86452d7785d82ed05fabf80f482c1ce0
mykytas <m.o.strelnyk@student.khai.edu>
2023-11-20 11:13:49 +0200

    stage 1 done

---
+++ src/CreditCalculator/Example.java
@@ -0,0 +1,9 @@
+package CreditCalculator;
+public class Example {
+    public void printExample() {
+        System.out.println("Loan principal: 1000");
+        System.out.println("Month 1: repaid 250");
+        System.out.println("Month 1: repaid 250");
+        System.out.println("Month 1: repaid 500");
+    }
+}
---
+++ src/CreditCalculator/Main.java
@@ -0,0 +1,8 @@
+package CreditCalculator;
+import org.apache.commons.cli.*;
+public class Main {
+    public static void main(String[] args){
+        Example example = new Example();
+        example.printExample();
+    }
+}
